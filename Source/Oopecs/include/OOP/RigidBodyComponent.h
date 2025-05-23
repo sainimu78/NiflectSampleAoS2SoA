3a214815520092058adee26fb4617d2fbc662051
sainimu78 <393584307@qq.com>
2025-05-23 23:31:09 +0800

    补测试类型

--- Source/Oopecs/include/OOP/RigidBodyComponent.h
+++ Source/Oopecs/include/OOP/RigidBodyComponent.h
@@ -1,10 +1,15 @@
 #pragma once
+#include "Niflect/Default/DefaultMacroTag.h"
 #include "OOP/Component.h"
+#include "Math/Vector3.h"
 
 namespace OOP
 {
+	NIF_T()
 	class CRigidBodyComponent : public CComponent
 	{
-
+	public:
+		NIF_F()
+		CVector3 m_velocity;
 	};
 }
--- Source/Oopecs/src/Main.cpp
+++ Source/Oopecs/src/Main.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "OOP/TransformComponent.h"
 #include "Oopecs_private.h"
 
 using namespace Niflect;
@@ -9,17 +8,33 @@ int main(int argc, char** argv)
 	auto reg = GetModuleRegistry();
 	reg->InitRegisteredModules();
 	using namespace OOP;
-	auto type = StaticGetType<CTransformComponent>();
-	CTransformComponent src;
-	src.m_position.m_x = 1;
-	src.m_position.m_y = 2;
-	src.m_position.m_z = 3;
-	CRwNode rw;
-	type->SaveInstanceToRwNode(&src, &rw);
-	CTransformComponent dst;
-	type->LoadInstanceFromRwNode(&dst, &rw);
-	ASSERT(src.m_position.m_x == dst.m_position.m_x);
-	ASSERT(src.m_position.m_y == dst.m_position.m_y);
-	ASSERT(src.m_position.m_z == dst.m_position.m_z);
+	{
+		auto type = StaticGetType<CTransformComponent>();
+		CTransformComponent src;
+		src.m_position.m_x = 1;
+		src.m_position.m_y = 2;
+		src.m_position.m_z = 3;
+		CRwNode rw;
+		type->SaveInstanceToRwNode(&src, &rw);
+		CTransformComponent dst;
+		type->LoadInstanceFromRwNode(&dst, &rw);
+		ASSERT(src.m_position.m_x == dst.m_position.m_x);
+		ASSERT(src.m_position.m_y == dst.m_position.m_y);
+		ASSERT(src.m_position.m_z == dst.m_position.m_z);
+	}
+	{
+		auto type = StaticGetType<CRigidBodyComponent>();
+		CRigidBodyComponent src;
+		src.m_velocity.m_x = 1;
+		src.m_velocity.m_y = 2;
+		src.m_velocity.m_z = 3;
+		CRwNode rw;
+		type->SaveInstanceToRwNode(&src, &rw);
+		CRigidBodyComponent dst;
+		type->LoadInstanceFromRwNode(&dst, &rw);
+		ASSERT(src.m_velocity.m_x == dst.m_velocity.m_x);
+		ASSERT(src.m_velocity.m_y == dst.m_velocity.m_y);
+		ASSERT(src.m_velocity.m_z == dst.m_velocity.m_z);
+	}
 	return 0;
 }
