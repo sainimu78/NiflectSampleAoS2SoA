0b434b439fa035ca8e90f8410c7f998d55ce7663
sainimu78 <393584307@qq.com>
2025-05-26 00:27:52 +0800

    修复 Linux 下错误

--- Source/AoS2SoA/src/ECS/Accessor/ArchecomponentHandleAccessor.h
+++ Source/AoS2SoA/src/ECS/Accessor/ArchecomponentHandleAccessor.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "ECS/ArchecomponentHandle.h"
+#include "OOP/Accessor/Vector3Accessor.h"
 
 namespace ECS
 {
@@ -8,16 +9,17 @@ namespace ECS
 	template <typename THandle>
 	class TArchecomponentHandleAccessor : public Niflect::CNiflectAccessor
 	{
+		typedef typename THandle::TArchetype TArchetype;
 	protected:
 		virtual bool SaveInstanceImpl(const InstanceType* base, CRwNode* rw) const override
 		{
-			auto& instance = *static_cast<const THandle::TArchetype*>(base);
+			auto& instance = *static_cast<const TArchetype*>(base);
 			RwOverloads::SaveToRwNode(instance, rw);
 			return true;
 		}
 		virtual bool LoadInstanceImpl(InstanceType* base, const CRwNode* rw) const override
 		{
-			auto& instance = *static_cast<THandle::TArchetype*>(base);
+			auto& instance = *static_cast<TArchetype*>(base);
 			ASSERT(rw->IsValue());
 			RwOverloads::LoadFromRwNode(rw, instance);
 			return true;
